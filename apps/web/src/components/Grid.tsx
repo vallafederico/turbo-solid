bf0edea4517f38a4b04e9594a8a5ee3822fb3ae8
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-02-02 16:07:37 +0100

    grid and localstorage grid.tsx

--- apps/web/src/components/Grid.tsx
+++ apps/web/src/components/Grid.tsx
@@ -1,7 +1,35 @@
-import { createEffect, createSignal } from "solid-js";
+import { createEffect, createSignal, onCleanup, onMount } from "solid-js";
+import { isClient } from "~/utils/isClient";
+
+function getGridValues() {
+  const computed = getComputedStyle(document.documentElement);
+  const gx = computed.getPropertyValue("--gx");
+  const gutter = computed.getPropertyValue("--gutter");
+  const columns = computed.getPropertyValue("--columns");
+
+  return { gx, gutter, columns };
+}
 
 export default function Grid({}) {
-  const num = Array.from({ length: 12 });
+  const [num, setNum] = createSignal(Array.from({ length: 12 }));
+
+  const handleResize = () => {
+    const { columns } = getGridValues(); // gx, gutter,
+    setNum(Array.from({ length: +columns }));
+  };
+
+  onMount(() => {
+    handleResize();
+    if (isClient) window.addEventListener("resize", handleResize);
+
+    // get from localstorage
+    const grid = localStorage.getItem("grid");
+    if (grid) setVisible(grid === "true");
+  });
+
+  onCleanup(() => {
+    if (isClient) window.removeEventListener("resize", handleResize);
+  });
 
   const [visible, setVisible] = createSignal(false);
 
@@ -10,6 +38,8 @@ export default function Grid({}) {
 
     if (e.key === "G") {
       setVisible(!visible());
+      // set to localstorage
+      localStorage.setItem("grid", visible().toString());
     }
   };
 
@@ -18,11 +48,11 @@ export default function Grid({}) {
   });
 
   const styles =
-    "gap-gutter fixed pointer-events-none left-0 top-0 z-10 flex h-[100vh] w-screen justify-between px-gx ";
+    "gap-[1rem] fixed pointer-events-none left-0 top-0 z-10 flex h-[100vh] w-screen justify-between px-gx ";
 
   return (
     <div class={visible() ? styles : "invisible"}>
-      {num.map((item) => {
+      {num().map((item) => {
         return <div class="grow bg-red-500 opacity-10"></div>;
       })}
     </div>
--- apps/web/src/routes/about/(about).tsx
+++ apps/web/src/routes/about/(about).tsx
@@ -14,13 +14,15 @@ export default function About() {
       <Section class="px-gx py-20">
         <h1 use:animateAlpha>About</h1>
       </Section>
-      <Section class="flex-center h-[80svh] w-full px-gx">
-        <h1 use:animateAlpha>One</h1>
+      <Section class="px-gx h-[80svh] w-full">
+        <h1 class="md:ml-grid-3 md:w-grids-4 border" use:animateAlpha>
+          One
+        </h1>
       </Section>
-      <Section class="flex-center h-[80svh] w-full px-gx">
+      <Section class="flex-center px-gx h-[80svh] w-full">
         <h1 use:animateAlpha>Two</h1>
       </Section>
-      <Section class="flex-center h-[80svh] w-full px-gx">
+      <Section class="flex-center px-gx h-[80svh] w-full">
         <h1 use:animateAlpha>Last</h1>
       </Section>
     </main>
