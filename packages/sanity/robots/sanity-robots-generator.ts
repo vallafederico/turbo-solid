259888e6af8c5edc53fa3bfccf061a9730c69bfa
Nathan Nye <nathan@nye.design>
2025-02-28 00:00:46 -0700

    cleanup

--- packages/sanity/robots/sanity-robots-generator.ts
+++ packages/sanity/robots/sanity-robots-generator.ts
@@ -1,12 +1,10 @@
 import sanityClient from "../client";
 
 export const generateSanityRobots = async () => {
-
-  const client = sanityClient
-  const document = await client.fetch('*[_type == "seo"][0]{robots}');
+  const document = await sanityClient.fetch('*[_type == "seo"][0]{robots}');
 
   if (document.robots?.length > 0) {
-    return document.robots.map((agent) => {
+    return document.robots.map((agent: any) => {
       return `User-agent: ${agent.userAgent}\nAllow: ${agent.allow}\nDisallow: ${agent.disallow}`;
     });
   }
