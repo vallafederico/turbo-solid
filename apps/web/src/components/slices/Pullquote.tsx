60bdbe06acc72bc7664642d98d378d651c2f1b90
Nathan Nye <nathan@nye.design>
2025-02-28 00:19:59 -0700

    idek brother

---
+++ apps/web/src/components/slices/Pullquote.tsx
@@ -0,0 +1,14 @@
+interface PullquoteProps {
+  text: string /* autogen */;
+  thingy: any /* autogen */;
+  age: number /* autogen */;
+  age2: number /* autogen */;
+}
+
+export default function Pullquote({ text, thingy, age, age2 }: PullquoteProps) {
+  return (
+    <div>
+      <h2>Pullquote Slice</h2>
+    </div>
+  );
+}
