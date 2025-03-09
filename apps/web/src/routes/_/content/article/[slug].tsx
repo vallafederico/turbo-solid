582264433c48b95e5151cde36bfa5db5d79e471b
Nathan Nye <nathan@nye.design>
2025-03-09 13:46:47 -0600

    slug

--- apps/web/src/routes/_/content/article/[slug].tsx
+++ apps/web/src/routes/_/content/article/[slug].tsx
@@ -11,18 +11,15 @@ export default function Article({ params }: { params: { slug: string } }) {
   const fetcher = createAsync(() => getDocumentBySlug("article", params.slug));
 
   return (
-    <div>
-      stuff
-      <SanityPage fetcher={fetcher}>
-        {(data) => {
-          return (
-            <div class="w-grids-6 mx-auto mt-[20vh]" use:animateAlpha>
-              <h1 class="mb-3r text-3xl font-bold">{data.title}</h1>
-              <PortableText value={data.body} />
-            </div>
-          );
-        }}
-      </SanityPage>
-    </div>
+    <SanityPage fetcher={fetcher}>
+      {(data) => {
+        return (
+          <div class="w-grids-6 mx-auto mt-[20vh]" use:animateAlpha>
+            <h1 class="mb-3r text-3xl font-bold">{data.title}</h1>
+            <PortableText value={data.body} />
+          </div>
+        );
+      }}
+    </SanityPage>
   );
 }
