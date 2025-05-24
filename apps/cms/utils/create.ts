3c968d5283d821d1b295df0076e6f65377268524
Nathan Nye <nathan@nye.design>
2025-05-24 08:07:27 -0600

    stuff

--- apps/cms/utils/create.ts
+++ apps/cms/utils/create.ts
@@ -1,130 +1,141 @@
 // import f
 
-import {FunctionComponent} from 'react'
-import {TbSection} from 'react-icons/tb'
-import {CardTone} from '@sanity/ui'
+import { FunctionComponent } from 'react'
+import { TbSection } from 'react-icons/tb'
+import { CardTone } from '@sanity/ui'
 
-export const createField = (name: string, type?: string, title?: string, extra?: any) => {
-  return {
-    name,
-    type,
-    title,
-    ...extra,
-  }
+export const createField = (
+	name: string,
+	type?: string,
+	title?: string,
+	extra?: any,
+) => {
+	return {
+		name,
+		type,
+		title,
+		...extra,
+	}
 }
 
 type TaxoCreatorFunction = {
-  name: string
-  title: string
-  fields: Array<object>
-  extra?: {[key: string]: any}
+	name: string
+	title: string
+	fields: Array<object>
+	extra?: { [key: string]: any }
 }
 
-export const createTaxo = ({name, title, fields, icon, ...extra}: TaxoCreatorFunction) => {
-  return {
-    name,
-    title,
-    type: 'document',
-    fields: [...fields],
-    icon,
-    ...extra,
-  }
+export const createTaxo = ({
+	name,
+	title,
+	fields,
+	icon,
+	...extra
+}: TaxoCreatorFunction) => {
+	return {
+		name,
+		title,
+		type: 'document',
+		fields: [...fields],
+		icon,
+		...extra,
+	}
 }
 
 type SliceSetCreatorFunction = {
-  name: string
-  title: string
-  slices: Array<object> | Set<object>
-  description?: string
-  options?: object
-  isLocked?: boolean
-  components?: {
-    input?: FunctionComponent
-    preview?: FunctionComponent
-    item?: FunctionComponent
-  }
+	name: string
+	title: string
+	slices: Array<object> | Set<object>
+	description?: string
+	options?: object
+	isLocked?: boolean
+	components?: {
+		input?: FunctionComponent
+		preview?: FunctionComponent
+		item?: FunctionComponent
+	}
 }
 
 const sortAlpha = (a: any, b: any) => {
-  if (!a.name || !b.name) return false
-  return a.name.localeCompare(b.name)
+	if (!a.name || !b.name) return false
+	return a.name.localeCompare(b.name)
 }
 
 const dedupe = (slices: any[]) => {
-  return [...new Set(slices)]
+	return [...new Set(slices)]
 }
 
 const organizeSlices = (slices: any[]) => {
-  const sortedAlpha = slices
-    .sort((a, b) => sortAlpha(a, b))
-    .map((slice) => ({
-      type: slice.name,
-      icon: slice.icon || TbSection, // Default the icon to this if there isnt one
-      // components: {
-      //   item: SliceItem,
-      // },
-    }))
+	const sortedAlpha = slices
+		.sort((a, b) => sortAlpha(a, b))
+		.map((slice) => ({
+			type: slice.name,
+			icon: slice.icon || TbSection, // Default the icon to this if there isnt one
+			// components: {
+			//   item: SliceItem,
+			// },
+		}))
 
-  return dedupe(sortedAlpha) as any[]
+	return dedupe(sortedAlpha) as any[]
 }
 
 export const createSliceSet = ({
-  name,
-  title = 'Page Slices',
-  slices = [],
-  description = 'Each section of the page can be edited here',
-  isLocked,
-  ...rest
+	name,
+	title = 'Page Slices',
+	slices = [],
+	description = 'Each section of the page can be edited here',
+	isLocked,
+	...rest
 }: SliceSetCreatorFunction) => {
-  return {
-    name,
-    title,
-    // components: {
-    //   input: isLocked && LockedArrayInput,
-    // },
-    description,
-    type: 'array',
-    of: slices?.length > 0 ? [...organizeSlices(slices)] : [],
-    ...rest,
-    options: {
-      insertMenu: {
-        views: [
-          {
-            name: 'grid',
-            // also takes a {groups} array of strings
-            previewImageUrl: (schemaTypeName: string) =>
-              `/static/slice-images/${schemaTypeName}.png`,
-          },
-        ],
-      },
-    },
-  }
+	return {
+		name,
+		title,
+		// components: {
+		//   input: isLocked && LockedArrayInput,
+		// },
+		description,
+		type: 'array',
+		of: slices?.length > 0 ? [...organizeSlices(slices)] : [],
+		...rest,
+		options: {
+			insertMenu: {
+				views: [
+					{
+						name: 'grid',
+						// also takes a {groups} array of strings
+						previewImageUrl: (schemaTypeName: string) =>
+							`/static/slice-images/${schemaTypeName}.png`,
+					},
+				],
+			},
+		},
+	}
 }
 
 type NoteCreateorFunction = {
-  title?: string
-  description: string
-  icon?: FunctionComponent
-  tone?: CardTone
-  rest?: any
+	title?: string
+	description: string
+	icon?: FunctionComponent
+	tone?: CardTone
+	rest?: any
 }
 
 export const createNote = ({
-  title,
-  description,
-  icon,
-  tone = 'primary',
-  ...rest
+	title,
+	description,
+	icon,
+	tone = 'primary',
+	...rest
 }: NoteCreateorFunction) => {
-  return {
-    name: 'note',
-    title,
-    type: 'note',
-    description,
-    options: {
-      icon,
-      tone,
-    },
-    ...rest,
-  }
+	return {
+		name: 'note',
+		title,
+		type: 'note',
+		description,
+		options: {
+			icon,
+			tone,
+		},
+		...rest,
+	}
 }
