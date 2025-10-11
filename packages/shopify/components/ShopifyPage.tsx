786d7f61a7adca71ac30e2199b02b6628d9901ae
Nathan Nye <nathan@nye.design>
2025-10-11 16:23:56 +0200

    page

--- packages/shopify/components/ShopifyPage.tsx
+++ packages/shopify/components/ShopifyPage.tsx
@@ -1,36 +1,36 @@
-import { ErrorBoundary, type JSX, Show } from "solid-js";
-import { HttpStatusCode } from "@solidjs/start";
-import type { Seo } from "../types";
-import { PageMeta } from "@local/solid-components";
+// import { ErrorBoundary, type JSX, Show } from "solid-js";
+// import { HttpStatusCode } from "@solidjs/start";
+// import type { Seo } from "../types";
+// import { PageMeta } from "@local/solid-components";
 
-export default function ShopifyPage({
-	children,
-	fetcher = () => {},
-	class: className,
-}: {
-	children: (data: any) => JSX.Element;
-	fetcher?: any;
-	class?: string;
-}) {
-	return (
-		<ErrorBoundary fallback={<HttpStatusCode code={404} />}>
-			<div class={`min-h-screen ${className}`}>
-				<Show when={fetcher()}>
-					{(data) => {
-						const seo = data()?.seo;
+// export default function ShopifyPage({
+// 	children,
+// 	fetcher = () => {},
+// 	class: className,
+// }: {
+// 	children: (data: any) => JSX.Element;
+// 	fetcher?: any;
+// 	class?: string;
+// }) {
+// 	return (
+// 		<ErrorBoundary fallback={<HttpStatusCode code={404} />}>
+// 			<div class={`min-h-screen ${className}`}>
+// 				<Show when={fetcher()}>
+// 					{(data) => {
+// 						const seo = data()?.seo;
 
-						const title = seo?.title || data()?.title;
-						const description = seo?.description || data()?.description;
+// 						const title = seo?.title || data()?.title;
+// 						const description = seo?.description || data()?.description;
 
-						return (
-							<>
-								<PageMeta title={title} description={description} />
-								{children(data)}
-							</>
-						);
-					}}
-				</Show>
-			</div>
-		</ErrorBoundary>
-	);
-}
+// 						return (
+// 							<>
+// 								<PageMeta title={title} description={description} />
+// 								{children(data)}
+// 							</>
+// 						);
+// 					}}
+// 				</Show>
+// 			</div>
+// 		</ErrorBoundary>
+// 	);
+// }
