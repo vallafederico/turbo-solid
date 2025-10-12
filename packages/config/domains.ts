063a81bee67cd1ade8bd7e6c8177740a20bf13d7
Nathan Nye <nathan@nye.design>
2025-10-12 18:54:51 +0200

    stuff

--- packages/config/domains.ts
+++ packages/config/domains.ts
@@ -1,6 +1,7 @@
 const DEV_DOMAIN = "http://localhost:3000";
 const PROD_DOMAIN =
-	process.env.VERCEL_URL || "https://internetthings-starter.vercel.app";
+	process.env.VERCEL_URL ||
+	"https://turbo-solid-web-git-feat-sanity-live-editing-federicoooo.vercel.app";
 
 export const DOMAIN =
 	process.env.NODE_ENV === "production" ? PROD_DOMAIN : DEV_DOMAIN;
