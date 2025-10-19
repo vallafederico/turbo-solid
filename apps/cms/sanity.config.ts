5dc8ecb6028c2c50b8f1f10953d7b539def689b6
Nathan Nye <nathan@nye.design>
2025-10-19 10:45:52 +0200

    update config

--- apps/cms/sanity.config.ts
+++ apps/cms/sanity.config.ts
@@ -10,18 +10,7 @@ import { userGuidePlugin } from "@q42/sanity-plugin-user-guide";
 import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
 import { userGuideStructure } from "./guides/userGuideStructure";
 
-import { SANITY } from "../../config";
 import { schemaMarkupPlugin } from "./plugins/schema-markup";
-import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
-import { schemaTypes } from "./schemas";
-import { media } from "sanity-plugin-media";
-import { structure } from "./desk/structure";
-import Logo from "./components/Logo";
-import { noteField } from "sanity-plugin-note-field";
-import { userGuidePlugin } from "@q42/sanity-plugin-user-guide";
-import { userGuideStructure } from "./guides/userGuideStructure";
 import { resolve } from "./resolve";
 import { DOMAIN, SANITY_CONFIG } from "@local/config";
 
@@ -38,6 +27,18 @@ const sharedConfig = [
 	userGuidePlugin({ userGuideStructure }),
 	vercelDeployTool(),
 	schemaMarkupPlugin(),
+	// presentationTool({
+	// 	previewUrl: {
+	// 		initial: DOMAIN,
+	// 		origin: DOMAIN,
+	// 		previewMode: {
+	// 			enable: "/api/preview-enable",
+	// 			disable: "/api/preview-disable",
+	// 		},
+	// 	},
+	// 	allowOrigins: ["http://localhost:*", DOMAIN],
+	// 	resolve,
+	// }),
 ];
 
 const devConfig = [visionTool()];
