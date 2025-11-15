8695c99abdb5e3acfb53d5ff606173a468cefd5e
Nathan Nye <nathan@nye.design>
2025-11-15 13:24:06 -0500

    domain trio

--- packages/config/domains.ts
+++ packages/config/domains.ts
@@ -1,7 +1,12 @@
 const DEV_DOMAIN = "http://localhost:3000";
+const STAGING_DOMAIN = "https://stuf.vercel.app";
 const PROD_DOMAIN =
 	process.env.VERCEL_URL ||
 	"https://turbo-solid-web-git-feat-sanity-live-editing-federicoooo.vercel.app";
 
 export const DOMAIN =
-	process.env.NODE_ENV === "production" ? PROD_DOMAIN : DEV_DOMAIN;
+	process.env.NODE_ENV === "development"
+		? DEV_DOMAIN
+		: process.env.NODE_ENV === "staging"
+			? STAGING_DOMAIN
+			: PROD_DOMAIN;
