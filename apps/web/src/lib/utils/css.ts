f98944df5114b7ddf1ba4b9b8e64c1c9885d7785
Nathan Nye <nathan@nye.design>
2025-11-16 12:31:46 -0500

    server

--- apps/web/src/lib/utils/css.ts
+++ apps/web/src/lib/utils/css.ts
@@ -1,11 +1,13 @@
+import { isServer } from "solid-js/web";
+
 export function getCssVariable(name: string): string | null {
-	if (typeof window === "undefined" || !document.documentElement) return null;
+	if (isServer || !document.documentElement) return null;
 	return getComputedStyle(document.documentElement)
 		.getPropertyValue(name)
 		.trim();
 }
 
 export function setCssVariable(name: string, value: string): void {
-	if (typeof window === "undefined" || !document.documentElement) return;
+	if (isServer || !document.documentElement) return;
 	document.documentElement.style.setProperty(name, value);
 }
