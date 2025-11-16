94f5737072ca804cda13dfb2ab632401211f0453
Nathan Nye <nathan@nye.design>
2025-11-16 18:53:07 -0500

    remove storybook next

--- packages/ui/components/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ label = "Button" }: { label: string }) {
-	return (
-		<button type="button" class="bg-blue-500 text-white px-4 py-2 rounded-md">
-			{label}
-		</button>
-	);
-}
