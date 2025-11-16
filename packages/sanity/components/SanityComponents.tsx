0e26827e9003460dfcf87227f54a2aea2096bb47
Nathan Nye <nathan@nye.design>
2025-11-16 18:20:18 -0500

    cleanup

--- packages/sanity/components/SanityComponents.tsx
+++ packages/sanity/components/SanityComponents.tsx
@@ -1,28 +1,28 @@
-import type { Component, JSX } from "solid-js"
-import { Dynamic, For} from 'solid-js/web'
+import type { Component, JSX } from "solid-js";
+import { Dynamic, For } from "solid-js/web";
 
 type ComponentList = {
-  [key: string]: Component<any>
-}
+	[key: string]: Component<any>;
+};
 
-
-export default function SanityComponents({ components, componentList }: { components: any[], componentList: ComponentList }) {
-  return (
-    <>
-      <For each={components}>
-        {(component) => {
-          if(!componentList[component._type]) {
-            console.warn(`Component of type ${component._type} not found`)
-            return null
-          }
-          return (
-            <Dynamic
-              component={componentList[component._type]}
-              {...component}
-            />
-          )
-        }}
-      </For>
-    </>
-  )
+export default function SanityComponents({
+	components,
+	componentList,
+}: {
+	components: any[];
+	componentList: ComponentList;
+}) {
+	return (
+		<For each={components}>
+			{(component) => {
+				if (!componentList[component._type]) {
+					console.warn(`Component of type ${component._type} not found`);
+					return null;
+				}
+				return (
+					<Dynamic component={componentList[component._type]} {...component} />
+				);
+			}}
+		</For>
+	);
 }
